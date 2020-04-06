8269462521aae4f63137c546a3b06906a40b487a
regevaital <regevav@ac.sce.ac.il>
2020-04-06 12:13:32 +0300

    account final change

--- mihraz7/account/models.py
+++ mihraz7/account/models.py
@@ -3,26 +3,38 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 
 class MyAccountManager(BaseUserManager):
-	def create_user(self, email, username, password=None):
+	def create_user(self, email, username, first_name, last_name, phone, password=None):
 		if not email:
 			raise ValueError('Users must have an email address')
 		if not username:
 			raise ValueError('Users must have a username')
-
+		if not first_name:
+			raise ValueError('Users must have first name')
+		if not last_name:
+			raise ValueError('Users must have last name')
+		if not phone:
+			raise ValueError('Users must have phone')
+	
 		user = self.model(
 			email=self.normalize_email(email),
 			username=username,
+			first_name=first_name,
+			last_name=last_name,
+			phone=phone
 		)
 
 		user.set_password(password)
 		user.save(using=self._db)
 		return user
 
-	def create_superuser(self, email, username, password):
+	def create_superuser(self, email, username, first_name, last_name, phone, password):
 		user = self.create_user(
 			email=self.normalize_email(email),
 			password=password,
 			username=username,
+			first_name=first_name,
+			last_name=last_name,
+			phone=phone
 		)
 		user.is_admin = True
 		user.is_staff = True
@@ -40,11 +52,14 @@ class Account(AbstractBaseUser):
 	is_active				= models.BooleanField(default=True)
 	is_staff				= models.BooleanField(default=False)
 	is_superuser			= models.BooleanField(default=False)
+	first_name				= models.CharField(max_length=30)
+	last_name 				= models.CharField(max_length=30)
+	phone 					= models.CharField(max_length=30)
 
 
 
-	USERNAME_FIELD = 'email'
-	REQUIRED_FIELDS = ['username']
+	USERNAME_FIELD = 'username'
+	REQUIRED_FIELDS = ['email','first_name','last_name','phone']
 
 	objects = MyAccountManager()
 
