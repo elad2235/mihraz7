063b31acf63eb5015f54bd9cb2011795b2084cc8
sean1511 <sean1511@gmail.com>
2020-05-05 20:45:03 +0300

    home page fix

--- mihraz7/mihraz7/urls.py
+++ mihraz7/mihraz7/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from account import views as account_views
 
 from account.views import (
     registration_view
@@ -9,7 +10,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/',include('register.urls')),
     path('login/',include('account.urls')),
-    path('account/',include('account.urls'))
+    path('account/',include('account.urls')),
+    path('',account_views.login_user)
 ]
 
 
