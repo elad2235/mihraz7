050931ee948dc76a79cb6027594d37f8d97a5e9c
regevaital <regevav@ac.sce.ac.il>
2020-05-13 00:58:21 +0300

    PM2020T9-21 register to tender

--- mihraz7/tenders/models.py
+++ mihraz7/tenders/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 class Tender(models.Model):
     tender_name = models.CharField(max_length=50)
-    tender_id = models.CharField(max_length=50)
-    winner = models.CharField(max_length=50)
-    files = models.CharField(max_length=100)
-    online_payment = models.CharField(max_length=100)
-    url = models.CharField(max_length=100)
+    tender_id = models.CharField(max_length=50,unique=True)
+    winner = models.CharField(max_length=50, blank=True)
+    files = models.CharField(max_length=100, blank=True)
+    online_payment = models.CharField(max_length=100,blank=True)
+    url = models.CharField(max_length=100, blank=True)
     end_date = models.DateField(auto_now=False,auto_now_add=False,blank=True)
     update_date = models.DateField(auto_now=False,auto_now_add=False,blank=True)
 
