3d70bf4c82c308b29b4f6ecaf1d2b4205aaa5b7e
Elad2235 <elad2235@gmail.com>
2020-05-20 22:16:13 +0300

    added 2 integration tests

--- mihraz7/account/tests/test_integration_accounts.py
+++ mihraz7/account/tests/test_integration_accounts.py
@@ -10,6 +10,7 @@ from account.models import Account
 
 
 class TestingAccountsIntegration(unittest.TestCase):
+    
     def test_add_delete(self):
         account_instance = Account.objects.create_user(email='test_@test.com',username='test_',first_name='tes',last_name='ting',phone='111111111',password='testtest')
         account_instance.save()
@@ -17,10 +18,23 @@ class TestingAccountsIntegration(unittest.TestCase):
         account_instance.delete()
         self.assertNotEqual(None, retrieve.last_name)
 
-
     def test_connect_disconnect(self):
         client = Client()
         client.post('/account/login_user/',{'username':'TestMe','password':'1342'},follow=True)
         client.get('/account/logOut')
         response = client.get('/account/homePage')
         self.assertEqual(response.status_code, 301)
+
+
+    def test_admin_connect_add_user_disconnect(self):
+        client = Client()
+        client.post('/account/login_user/',{'username':'TestMe','password':'1342'},follow=True)
+        account_instance = Account.objects.create_user(email='test_@test.com',username='test_',first_name='tes',last_name='ting',phone='111111111',password='testtest')
+        account_instance.save()
+        retrieve = Account.objects.get(username='test_')
+        account_instance.delete()
+        self.assertNotEqual(None, retrieve.last_name)
+        client.get('/account/logOut')
+        response = client.get('/account/homePage')
+        self.assertEqual(response.status_code, 301)
+
--- mihraz7/tendersOffers/tests/test_integration_tenderOffers.py
+++ mihraz7/tendersOffers/tests/test_integration_tenderOffers.py
@@ -6,7 +6,7 @@ import xmlrunner
 django.setup()
 
 from tendersOffers.models import TenderOffer
-
+from tenders.models import Tender
 
 class TestingTendersOffersIntegration(unittest.TestCase):
     def test_add_delete_tendersOffers(self):
@@ -15,3 +15,19 @@ class TestingTendersOffersIntegration(unittest.TestCase):
         retrieve = TenderOffer.objects.get(tender_name='test')
         tenderoffer_instance.delete()
         self.assertNotEqual(None, retrieve.tender_id)
+
+
+    def test_new_tender_submit_offer_cancel_offer(self):
+        tender_instance = Tender.objects.create(tender_name='Tender1', tender_id='1234', winner='test1', files='file', online_payment='yes', url='test.com', end_date='2020-05-17', update_date='2020-05-14')
+        tender_instance.save()
+        retrieve_t = Tender.objects.get(tender_name='Tender1')
+        self.assertEqual('Tender1',retrieve_t.tender_name)
+
+        tenderoffer_instance = TenderOffer.objects.create(id=1, tender_id='1234', tender_name='Tender1', first_name='sean', last_name='sean', email='test@test.com', offer='100')
+        tenderoffer_instance.save()
+        retrieve_to = TenderOffer.objects.get(tender_name='Tender1')
+        tenderoffer_instance.delete()
+        self.assertNotEqual(None, retrieve_to.tender_id)
+
+        tender_instance.delete()
+        self.assertNotEqual(None, retrieve_t.tender_id)
