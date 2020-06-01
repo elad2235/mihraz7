481a0826d36fdc1c2dbde7f08a629a56c074785a
elad2235 <elad@DESKTOP-PH2GVJM.localdomain>
2020-06-01 22:40:19 +0300

    deploy attempt

--- mihraz7/mihraz7/settings.py
+++ mihraz7/mihraz7/settings.py
@@ -131,5 +131,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-#STATIC_ROOT = 'static'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = 'static'
+#STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
