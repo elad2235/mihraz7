e64d0fb69e61cb4f6f60e8f7cb525e6ae26778a9
sean1511 <sean1511@gmail.com>
2020-06-03 11:14:01 +0300

    unittest+integration test contactus app

---
+++ mihraz7/contact/tests/test_contact.py
@@ -0,0 +1,58 @@
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mihraz7.settings'
+import unittest
+import django
+import xmlrunner
+django.setup()
+from contact.models import contact 
+from django.test import Client
+
+
+class TestingContact(unittest.TestCase):
+    
+    def test_insert_name(self):
+        contact_instance = contact.objects.create(Name='Test1', Email='Test@test.com', Subject='testing', Message='Hello', date='2020-05-17')
+        contact_instance.save()
+        retrieve = contact.objects.get(Name='Test1')
+        self.assertEqual(contact_instance.Name, retrieve.Name)
+        retrieve.delete()
+
+
+    def test_insert_email(self):
+        contact_instance = contact.objects.create(Name='Test1', Email='Test@test.com', Subject='testing', Message='Hello', date='2020-05-17')
+        contact_instance.save()
+        retrieve = contact.objects.get(Email='Test@test.com')
+        self.assertEqual(contact_instance.Email, retrieve.Email)
+        retrieve.delete()
+
+
+    def test_insert_subject(self):
+        contact_instance = contact.objects.create(Name='Test1', Email='Test@test.com', Subject='testing', Message='Hello', date='2020-05-17')
+        contact_instance.save()
+        retrieve = contact.objects.get(Subject='testing')
+        self.assertEqual(contact_instance.Subject, retrieve.Subject)
+        retrieve.delete()
+
+    def test_insert_message(self):
+        contact_instance = contact.objects.create(Name='Test1', Email='Test@test.com', Subject='testing', Message='Hello', date='2020-05-17')
+        contact_instance.save()
+        retrieve = contact.objects.get(Message='Hello')
+        self.assertEqual(contact_instance.Message, retrieve.Message)
+        retrieve.delete()
+
+    def test_remove_contact(self):
+        contact_instance = contact.objects.create(Name='Test1', Email='Test@test.com', Subject='testing', Message='Hello', date='2020-05-17')
+        contact_instance.save()
+        contact_instance.delete()
+        try:
+            retrieve = contact.objects.get(Name='Test1')
+        except contact.DoesNotExist:
+            retrieve=None
+        self.assertEqual(None, retrieve)
+
+
+if __name__ == '__main__':
+    with open('test-reports/results.xml', 'wb') as output:
+        unittest.main(
+            testRunner=xmlrunner.XMLTestRunner(output=output),
+            failfast=False, buffer=False, catchbreak=False)
---
+++ mihraz7/contact/tests/test_integration_suppliers.py
@@ -0,0 +1,20 @@
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mihraz7.settings'
+import unittest
+import django
+import xmlrunner
+django.setup()
+
+from django.test import Client
+from contact.models import contact
+
+
+class TestingContactIntegration(unittest.TestCase):
+    def test_add_delete_contact(self):
+        contact_instance = contact.objects.create(Name='Test1', Email='Test@test.com', Subject='testing', Message='Hello', date='2020-05-17')
+        contact_instance.save()
+        retrieve = contact.objects.get(Name='Test1')
+        contact_instance.delete()
+        self.assertNotEqual(None, retrieve.Name)
+    
+
