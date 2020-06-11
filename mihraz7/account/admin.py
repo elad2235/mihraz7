25f4ba6a8c94aa590652d427274af58a4cfd6c59
elad2235 <elad2235@gmail.com>
2020-06-11 08:03:41 -0400

    fixed flake8 problems

--- mihraz7/account/admin.py
+++ mihraz7/account/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from .models import Account
 from django.contrib.auth.hashers import make_password
 
+
 class PasswordUserAdmin(admin.ModelAdmin):
-    def save_model(self,request,obj,form,change):
+    def save_model(self, request, obj, form, change):
         obj.password = make_password(obj.password)
         obj.user = request.user
         obj.save()
 
-admin.site.register(Account,PasswordUserAdmin)
+
+admin.site.register(Account, PasswordUserAdmin)
