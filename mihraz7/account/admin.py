828b55d3fa206c8bec02b06633e8abe59ca443e2
elad2235 <elad2235@gmail.com>
2020-06-11 07:57:46 -0400

    fixed password hash after change

--- mihraz7/account/admin.py
+++ mihraz7/account/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Account
+from django.contrib.auth.hashers import make_password
 
-admin.site.register(Account)
+class PasswordUserAdmin(admin.ModelAdmin):
+    def save_model(self,request,obj,form,change):
+        obj.password = make_password(obj.password)
+        obj.user = request.user
+        obj.save()
+
+admin.site.register(Account,PasswordUserAdmin)
